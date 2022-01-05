5dde08a041276bc05f72d446b4d67c5e43e47d27
Yasmin <yasmin.bunnie@gmail.com>
2022-01-05 23:12:05 +0000

    Testing: Prompt & New Function

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+function generatePassword() {
+  passwordLength = prompt(
+    "How many characters would you like your password to contain?"
+  );
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
